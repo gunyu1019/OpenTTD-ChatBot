a06e57381694e90e7305106a32147f3ed988b63d
gunyu1019 <gunyu1019@gmail.com>
2022-10-19 15:20:56 +0900

    edit chatting

--- cogs/chatting.py
+++ cogs/chatting.py
@@ -1,12 +1,14 @@
-import logging
+import os.path
+import re
 import json
 import discord
 from discord.ext import interaction
 from libottdadmin2.enums import Action, DestType, ChatAction
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 
 from client.discord_client import DiscordClient
 from config.config import get_config
+from utils.directory import directory
 
 parser = get_config()
 
@@ -16,36 +18,47 @@ class Chatting:
         self.bot = bot
 
         self.is_enabled = False
-        self.guild_id = -1
-        self.channel_id: List[int] = []
+        self.webhook: Optional[discord.Webhook] = None
+        self.webhook_url = parser.get("Discord", "webhook_url", fallback=None)
+        if self.webhook_url is not None:
+            m = re.search(
+                r'discord(?:app)?.com/api/webhooks/(?P<id>[0-9]{17,20})/(?P<token>[A-Za-z0-9\.\-\_]{60,68})',
+                self.webhook_url
+            )
+            if m is None:
+                raise ValueError('Invalid webhook URL given.')
 
-        self.guild: Optional[discord.Guild] = None
-        self.channels: List[discord.abc.MessageableChannel] = []
+            data: Dict[str, Any] = m.groupdict()
+            self.webhook_id = data['id']
+
+        with open(os.path.join(directory, "config", "profile_image.json"), "r") as fp:
+            self.profile_json = json.load(fp)
+
+    def get_avatar_url(self, username: str):
+        user_info = None
+        for profile in self.profile_json:
+            if username in profile['username']:
+                user_info = profile
+                break
+        else:
+            return
+
+        if username['profileType'] == 0:
+            member = self.bot.get_user(user_info['discordId'])
+            return member.display_avatar.url
 
     @interaction.listener()
     async def on_ready(self):
-        self.is_enabled = parser.has_option("Discord", "guild_id") and parser.has_option("Discord", "channel_id")
-        if self.is_enabled:
-            self.guild_id = parser.getint("Discord", "guild_id")
-            self.channel_id = list(
-                    map(int, list(
-                        json.loads(parser.get("Discord", "channel_id", fallback="[]"))
-                    )
-                )
-            )
-
-            self.guild = self.bot.get_guild(self.guild_id)
-            self.channels = [
-                self.guild.get_channel(x) for x in self.channel_id
-            ]
+        if self.webhook_url is not None:
+            self.is_enabled = True
+            self.webhook = self.bot.fetch_webhook(self.webhook_id)
 
     @interaction.listener()
     async def on_server_chat(self, action: Action, type: DestType, client_id: int, message: str, extra: int):
         client = self.bot.openttd_client.clients.get(client_id, client_id)
         client = getattr(client, "name", client)
         if type == DestType.BROADCAST and action == ChatAction.CHAT and self.is_enabled:
-            for channel in self.channels:
-                await channel.send("**[{0}]**: {1}".format(client, message))
+            await self.webhook.send(username=client, avatar_url=self.get_avatar_url(client), content=message)
         return
 
 
